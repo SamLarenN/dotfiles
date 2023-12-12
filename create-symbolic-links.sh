030b946c296e12fed51f8d21cc3926f7aebcdbd3
Samuel Skoog <ephixn@gmail.com>
2023-12-12 23:29:13 +0100

    Update symbolic links

--- create-symbolic-links.sh
+++ create-symbolic-links.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -sf ~/dotfiles/nvim ~/.config/nvim
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/nvim ~/.config/nvim
+ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
